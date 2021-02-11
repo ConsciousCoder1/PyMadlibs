8f2d5ab9d1b9c9fbff6b70847cf0bc0943089b85
ConsciousCoder1 <agpperez1@gmail.com>
2021-02-10 21:08:06 -0500

    Initial Commit

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+verb1 = input('Verb: ')
+noun1 = input('Noun: ')
+adj1 = input('Adjective: ')
+food = input('Food: ')
+adj2 = input('Adjective: ')
+adj3 = input('Adjective: ')
+celebrity = input('Celebrity: ')
+noun2 = input('Noun: ')
+verb2 = input('Verb: ')
+
+
+
+madlib = f"I really enjoy going to the movies. Before the movie starts, I like to {verb1} to the concession stand and grab some {noun1} to bring into the theater. I find it especially {adj1} to share {food} with my friends while we watch. My favorite kind of movie is {adj2} with a little {adj3} to spice things up. And I can't get enough of watching {celebrity}! Anyway, I would say, outside of that, my tip-top favorite genre is {noun2}. Whenever a new one comes out, I can't {verb2} to be there!"
+
+print(madlib)
